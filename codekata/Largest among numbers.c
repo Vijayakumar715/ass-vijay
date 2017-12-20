d0b0d0815cc857f462cc172e3fbb93fc6ecd45f2
Vijayakumar715 <34677114+Vijayakumar715@users.noreply.github.com>
2017-12-20 22:25:38 +0530

    Create Largest among numbers.c

---
+++ codekata/Largest among numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int a[50],max=0,i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+
+
+	
+}
+
