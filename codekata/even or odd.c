ac084b103537d4868fc3def027fb3941caf10252
Vijayakumar715 <34677114+Vijayakumar715@users.noreply.github.com>
2017-12-20 21:40:24 +0530

    Create even or odd.c

---
+++ codekata/even or odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int x;
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+        printf("Number is even");
+    }
+    else
+    {
+        printf("Number is odd");
+    }
+}
